7338360b5121592778a2e1fac7224081f2831f87
AliGates915 <hacktech877@gmail.com>
2024-10-28 04:47:00 -0700

    tets

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,8 +39,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true,
-     
-        // sameSite: "strict", // Controls cookie behavior for cross-site requests
+        secure: process.env.NODE_ENV === "production", // Ensure this is false if you're testing on HTTP
+        sameSite: "None", // Consider c
       })
       .status(200)
       .json({ ...otherDetails, isAdmin });
